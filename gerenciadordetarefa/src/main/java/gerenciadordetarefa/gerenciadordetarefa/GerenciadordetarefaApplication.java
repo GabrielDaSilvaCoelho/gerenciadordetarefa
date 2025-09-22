4bcd90774e873383a79ca7b1ba35063086ca7906
gabrieldaxrlx <gabrielcoelhosamsung@gmail.com>
2025-09-22 13:17:28 -0300

    feat:API de tarefas

--- gerenciadordetarefa/src/main/java/gerenciadordetarefa/gerenciadordetarefa/GerenciadordetarefaApplication.java
+++ gerenciadordetarefa/src/main/java/gerenciadordetarefa/gerenciadordetarefa/GerenciadordetarefaApplication.java
@@ -1,13 +1,23 @@
 package gerenciadordetarefa.gerenciadordetarefa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import gerenciadordetarefa.gerenciadordetarefa.Tarefa;
+import gerenciadordetarefa.gerenciadordetarefa.TarefaRepository;
+
 
 @SpringBootApplication
 public class GerenciadordetarefaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadordetarefaApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(TarefaRepository repository) {
+		return args -> {
+		};
+	}
 }
--- gerenciadordetarefa/src/main/java/gerenciadordetarefa/gerenciadordetarefa/TarefaRepository.java
+++ gerenciadordetarefa/src/main/java/gerenciadordetarefa/gerenciadordetarefa/TarefaRepository.java
@@ -1,4 +1,8 @@
 package gerenciadordetarefa.gerenciadordetarefa;
 
-public class TarefaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
